3d347edbc5f34b3b9e60a8ad920949d9686e5ce7
LeSteackHacher <119041317+LeSteackHacher@users.noreply.github.com>
2023-04-27 09:10:22 +0200

    Add files via upload
    Permet d'ajouter les objets à la base de données

---
+++ Objet.sql
@@ -0,0 +1,24 @@
+INSERT INTO `products` (`id`, `name`, `slug`, `short_description`, `description`, `price`, `quantity`, `image`, `cat_id`, `created_at`, `updated_at`)
+VALUES ('1', 'Jeep', 'jeep', 'Jouet', 'Puzzle en bois représentant une jeep. Taille : 36cm x 12cm', '25', '1', 'JeepBois.jpg', '1', '2023-04-18 11:28:27', '2023-04-19 11:28:27');
+INSERT INTO `products` (`id`, `name`, `slug`, `short_description`, `description`, `price`, `quantity`, `image`, `cat_id`, `created_at`, `updated_at`)
+VALUES ('2', 'Accordéon', 'accordeon', 'Instrument', 'Puzzle en bois représentant un accordéon. Taille : 36cm x 12cm', '50', '1', 'AccordeonBois.jpg', '3', '2023-04-18 11:28:27', '2023-04-19 11:28:27');
+INSERT INTO `products` (`id`, `name`, `slug`, `short_description`, `description`, `price`, `quantity`, `image`, `cat_id`, `created_at`, `updated_at`)
+VALUES ('3', 'Batterie', 'batterie', 'Instrument', 'Puzzle en bois représentant une batterie de musique. Taille : 36cm x 12cm', '43', '1', 'BatterieBois.png', '3', '2023-04-18 11:28:27', '2023-04-19 11:28:27');
+INSERT INTO `products` (`id`, `name`, `slug`, `short_description`, `description`, `price`, `quantity`, `image`, `cat_id`, `created_at`, `updated_at`)
+VALUES ('4', 'Guitare Electrique', 'guitare-electrique', 'Instrument', 'Puzzle en bois représentant une guitare électrique. Taille : 36cm x 12cm', '36', '1', 'GuitareBois.png', '3', '2023-04-18 11:28:27', '2023-04-19 11:28:27');
+INSERT INTO `products` (`id`, `name`, `slug`, `short_description`, `description`, `price`, `quantity`, `image`, `cat_id`, `created_at`, `updated_at`)
+VALUES ('5', 'Saxophone', 'saxophone', 'Instrument', 'Puzzle en bois représentant un saxophone. Taille : 36cm x 12cm', '50', '1', 'SaxoBois.png', '3', '2023-04-18 11:28:27', '2023-04-19 11:28:27');
+INSERT INTO `products` (`id`, `name`, `slug`, `short_description`, `description`, `price`, `quantity`, `image`, `cat_id`, `created_at`, `updated_at`)
+VALUES ('6', 'Horloge', 'horloge', 'Décoration', 'Un puzzle permettant de former une petite horloge de bois en forme de chouette. Taille : 36cm x 12cm', '75', '1', 'HorlogeBois.png', '2', '2023-04-18 11:28:27', '2023-04-19 11:28:27');
+INSERT INTO `products` (`id`, `name`, `slug`, `short_description`, `description`, `price`, `quantity`, `image`, `cat_id`, `created_at`, `updated_at`)
+VALUES ('7', 'Voiture Citrouille', 'voiture-citrouille', 'Jouet', 'Puzzle en bois représentant la voiture citrouille du conte de Cendrillon. Taille : 36cm x 12cm', '55', '1', 'CitrouilleBois.png', '1', '2023-04-18 11:28:27', '2023-04-19 11:28:27');
+INSERT INTO `products` (`id`, `name`, `slug`, `short_description`, `description`, `price`, `quantity`, `image`, `cat_id`, `created_at`, `updated_at`)
+VALUES ('8', 'Voiture de course', 'voiture-de-course', 'Jouet', 'Puzzle en bois représentant un voiture de course. Taille : 36cm x 12cm', '100', '10', 'CourseBois.png', '1', '2023-04-18 11:28:27', '2023-04-19 11:28:27');
+INSERT INTO `products` (`id`, `name`, `slug`, `short_description`, `description`, `price`, `quantity`, `image`, `cat_id`, `created_at`, `updated_ats`)
+VALUES ('9', 'Lapin Steampunk', 'lapin-steampunk', 'Décoration', 'Puzzle en bois représentant un lapin de style Steampunk. Taille : 36cm x 12cm', '33', '1', 'LapinBois.png', '2', '2023-04-18 11:28:27', '2023-04-19 11:28:27');
+INSERT INTO `products` (`id`, `name`, `slug`, `short_description`, `description`, `price`, `quantity`, `image`, `cat_id`, `created_at`, `updated_at`)
+VALUES ('10', 'Vaisseau Steampunk', 'vaisseau-steampunk', 'Décoration', 'Puzzle en bois représentant un vaisseau extraterrestre de style Steampunk. Taille : 36cm x 12cm', '45', '1', 'VaisseauBois.png', '2', '2023-04-18 11:28:27', '2023-04-19 11:28:27');
+INSERT INTO `products` (`id`, `name`, `slug`, `short_description`, `description`, `price`, `quantity`, `image`, `cat_id`, `created_at`, `updated_at`)
+VALUES ('11', 'Sous-Marin Steampunk', 'sous-marin-steampunk', 'Décoration', 'Puzzle en bois représentant un sous-marin de style Steampunk. Taille : 36cm x 12cm', '40', '1', 'MarinBois.png', '2', '2023-04-18 11:28:27', '2023-04-19 11:28:27');
+INSERT INTO `products` (`id`, `name`, `slug`, `short_description`, `description`, `price`, `quantity`, `image`, `cat_id`, `created_at`, `updated_at`)
+VALUES ('12', 'Camion', 'camion', 'Jouet', 'Puzzle en bois représentant un camion. Taille : 36cm x 12cm', '32', '10', 'CamionBois.png', '1', '2023-04-18 11:28:27', '2023-04-19 11:28:27');
